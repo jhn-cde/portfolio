1d1edca54b0d28946730f8dab213d055d1eb6ca0
jwilla <140156@unsaac.edu.pe>
2023-01-27 14:52:01 -0500

    roboto font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--roboto-font',
+ })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.variable}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
