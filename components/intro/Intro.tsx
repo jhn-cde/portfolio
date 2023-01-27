7045268e505ee36e93afbaf7f6b4a856cb236edd
jwilla <140156@unsaac.edu.pe>
2023-01-27 16:42:09 -0500

    intro componenet

---
+++ components/intro/Intro.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <>
+      <h1>Johan Huaman.</h1>
+      <h2 title='description'>Software Developer.</h2>
+      <p title='content'>I’m a recent computer science graduate with passion for programming. Seeking job opportunities to apply my skills and stay current with the latest technologies.</p>
+    </>
+  )
+}
+
+export default Intro
