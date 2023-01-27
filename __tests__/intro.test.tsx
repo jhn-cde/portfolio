6a6c94ec6ce3025ed3d1af3f24a7c4b1d018f1b8
jwilla <140156@unsaac.edu.pe>
2023-01-27 16:36:25 -0500

    Intro section tests

---
+++ __tests__/intro.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react'
+import Intro from '@/components/intro/Intro'
+
+describe('section Intro', () => {
+  it('renders a heading', () => {
+    render(<Intro />)
+
+    const heading = screen.getByRole('heading', 
+    {name: "Johan Huaman",})
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders a description', () => {
+    render(<Intro />)
+
+    const description = screen.getByTitle('description')
+
+    expect(description).toBeInTheDocument()
+  })
+
+  it('renders a content', () => {
+    render(<Intro />)
+
+    const content = screen.getByTitle('content')
+
+    expect(content).toBeInTheDocument()
+  })
+})
