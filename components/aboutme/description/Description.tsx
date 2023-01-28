b8dcd2f71e9f3f0575491103fcbc0aa89510153f
jwilla <140156@unsaac.edu.pe>
2023-01-28 12:53:25 -0500

    text styles

--- components/aboutme/description/Description.tsx
+++ components/aboutme/description/Description.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import styles from './description.module.css'
 
 const Description = () => {
   return (
     <div>
-      <p>
+      <p className={styles.text}>
         I am a recent graduate in Computer Science from the National University San Antonio Abad del Cusco. I specialize in software development and machine learning and have gained valuable experience through participating in coding competitions. 
       </p>
-      <p>
+      <p className={styles.text}>
         I am highly motivated with a passion for technology and a strong desire to learn and grow as a professional. I am a fast learner, adaptable and always willing to take on new challenges.
       </p>
-      <p>
+      <p className={styles.text}>
         As a recent graduate, I am eager to start my professional career and utilize the skills and knowledge I have acquired during my studies. I am open to remote or on-site opportunities and am willing to work from anywhere.
       </p>
-      <p>
-        Technologies I’ve been working with recently:
+      <p className={styles.text}>
+        Technologies I've been working with recently:
       </p>
     </div>
   )
