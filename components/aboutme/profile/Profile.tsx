1197a4623a272c52d145e91f1e520378560fc1ab
jwilla <140156@unsaac.edu.pe>
2023-01-28 15:34:31 -0500

    profile image

--- components/aboutme/profile/Profile.tsx
+++ components/aboutme/profile/Profile.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
-
+import styles from './profile.module.css'
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className={styles.container}>
+      <Image
+        src={'/images/profile.png'}
+        width={500}
+        height={441}
+        alt='johan photo'
+        className={styles.img}
+      />
+    </div>
   )
 }
 
