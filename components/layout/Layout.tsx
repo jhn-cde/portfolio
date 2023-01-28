38f86207ac7ed60ca9db37eee4f9b9f280e35c33
jwilla <140156@unsaac.edu.pe>
2023-01-28 00:44:05 -0500

    handle scroll, resize

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import React from 'react'
+import Image from 'next/image';
+import React, { use, useEffect, useState } from 'react'
 import styles from './layout.module.css';
 
 interface Props{
@@ -9,6 +10,26 @@ interface Props{
 export const siteTitle = 'Mi Portafolio';
 
 const Layout = ({ children }: Props) => {
+  const [navbarScroll, setNavbarScroll] = useState(false)
+  const [windowWidth, setWindowWidth] = useState(1000)
+  const [windowHeigth, setWindowHeigth] = useState(1800)
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    window.addEventListener("resize", handleResize)
+  }, [])
+  
+  const handleScroll = () => {
+    setNavbarScroll(window.scrollY>=70)
+  }
+
+  const handleResize = () => {
+    const _width = window.innerWidth-40
+    const _height = _width*1800/1000
+    setWindowWidth(_width)
+    setWindowHeigth(_height)
+  }
+
   return (
     <div>
       <Head>
@@ -27,9 +48,22 @@ const Layout = ({ children }: Props) => {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
-      <header title='header' className={styles.header}><p>header</p></header>
-      <div title='content'>{children}</div>
-      <footer title='footer'></footer>
+      <header title='header' className={navbarScroll?styles.header_scroll:styles.header_container}><p>header</p></header>
+      
+      <div title='content' className={styles.content}>{children}</div>
+      
+      <footer className={styles.footer} title='footer'></footer>
+
+      <div className={styles.image_container}>
+        <Image 
+          className={styles.image}
+          src="/images/background.svg"
+          width={windowWidth}
+          height={windowHeigth}
+          alt=''
+          priority
+        />
+      </div>
 
     </div>
   )
