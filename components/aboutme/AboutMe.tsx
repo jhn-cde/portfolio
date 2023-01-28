6e1b72612c2af6bb06d02fe702eb0dced626367d
jwilla <140156@unsaac.edu.pe>
2023-01-28 10:32:17 -0500

    about me component

--- components/aboutme/AboutMe.tsx
+++ components/aboutme/AboutMe.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import styles from './aboutme.module.css'
+import Description from './description/Description'
+import Profile from './profile/Profile'
 
 const AboutMe = () => {
   return (
-    <section>
-      <h2>About Me</h2>
+    <section className={styles.section}>
+      <div className={styles.title_container}>
+        <h2 className={styles.title}>About Me</h2>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.profile}>
+          <Profile />
+        </div>
+        <div className={styles.description}>
+          <Description />
+        </div>
+      </div>
     </section>
   )
 }
---
+++ components/aboutme/description/Description.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Description = () => {
+  return (
+    <div>
+      <p>
+        I am a recent graduate in Computer Science from the National University San Antonio Abad del Cusco. I specialize in software development and machine learning and have gained valuable experience through participating in coding competitions. 
+      </p>
+      <p>
+        I am highly motivated with a passion for technology and a strong desire to learn and grow as a professional. I am a fast learner, adaptable and always willing to take on new challenges.
+      </p>
+      <p>
+        As a recent graduate, I am eager to start my professional career and utilize the skills and knowledge I have acquired during my studies. I am open to remote or on-site opportunities and am willing to work from anywhere.
+      </p>
+      <p>
+        Technologies I’ve been working with recently:
+      </p>
+    </div>
+  )
+}
+
+export default Description
