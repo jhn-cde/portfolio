4bbc1362b53a4994aac86d3e3db9ddbac49dda32
jwilla <140156@unsaac.edu.pe>
2023-01-28 15:36:22 -0500

    backgorund styles

--- components/background/Background.tsx
+++ components/background/Background.tsx
@@ -1,28 +1,18 @@
 import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styles from './background.module.css';
 
 const Background = () => {
-  const [size, setSize] = useState({width: 1000, height: 1800})
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize)
-  }, [])
-
-  const handleResize = () => {
-    const _width = window.innerWidth-60
-    const _height = _width*1800/1000
-    setSize({width: _width, height: _height})
-  }
   
   return (
     <div className={styles.container}>
       <Image 
         src="/images/background.svg"
-        width={size.width}
-        height={size.height}
+        width={1000}
+        height={1800}
         alt='background'
         priority
+        className={styles.img}
       />
     </div>
   )
