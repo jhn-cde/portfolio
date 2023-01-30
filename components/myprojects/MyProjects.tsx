6acaa245f2de8d1aac0c9029f80fe865fa4c91b0
jwilla <140156@unsaac.edu.pe>
2023-01-30 18:09:34 -0500

    styles

--- components/myprojects/MyProjects.tsx
+++ components/myprojects/MyProjects.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './myprojects.module.css'
 
 const MyProjects = () => {
   return (
     <section>
-      <div className="container">
-        <div className="title_container">
-          <h2 className="title">
+      <div className={styles.container + ' container'}>
+        <div className={styles.title_container}>
+          <h2 className={styles.title} id={'myprojects'}>
             Some Of My Projects
           </h2>
         </div>
