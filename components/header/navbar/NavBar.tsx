eade5e8151f62cf21f41c4209265fbfd5de67fde
jwilla <140156@unsaac.edu.pe>
2023-02-03 23:19:31 -0500

    logo

--- components/header/navbar/NavBar.tsx
+++ components/header/navbar/NavBar.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
 import styles from './navbar.module.css'
-
-const NavBar = () => {
+interface Props{
+  scroll: boolean
+}
+const NavBar = ({scroll}: Props) => {
+  const [logo, setLogo] = useState('/images/logo.svg')
+  useEffect(() => {
+    if(scroll)
+      setLogo('/images/logo_scroll.svg')
+    else
+      setLogo('/images/logo.svg')
+  }, [scroll])
+  
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
-        Logo
+        <Image
+          src={logo}
+          width={50}
+          height={50}
+          alt='plan de estudios'
+          className={scroll?styles.scroll:styles.img}
+        />
       </div>
       <div className={styles.menu}>
         menu
