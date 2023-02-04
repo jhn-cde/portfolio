82ea61b4d2494ff2163751468bd0359ac2708fce
jwilla <140156@unsaac.edu.pe>
2023-02-03 23:19:59 -0500

    navbar scroll props

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <header title='header' className={navbarScroll?styles.header_scroll:styles.header}
     >
-      <NavBar/>
+      <NavBar scroll={navbarScroll}/>
     </header>
   )
 }
