63b83b9bfec492779169c81c74d9a1c0f71d3076
jwilla <140156@unsaac.edu.pe>
2023-02-05 15:27:49 -0500

    h4 to span

--- components/sections/intro/Intro.tsx
+++ components/sections/intro/Intro.tsx
@@ -7,7 +7,7 @@ const Intro = () => {
       <div className="w-auto sm:w-10/12">
         <h6 className="text-accent sm:text-lg font-bold font-mono">Hi there! I'm</h6>
         <h1 className='text-accent text-2xl sm:text-5xl font-black'>Johan Huaman.</h1>
-        <h2 className="text-secondary text-2xl sm:text-5xl font-bold" title='description'>Software Developer.</h2>
+        <span className="text-secondary text-2xl sm:text-5xl font-bold" title='description'>Software Developer.</span>
         <p title='content' className="text-secondary">I’m a recent computer science graduate with passion for programming. Seeking job opportunities to apply my skills and stay current with the latest technologies.</p>
       </div>
       <div className="w-1/2 h-3/4 border-10 bg-secondary opacity-80"/>
