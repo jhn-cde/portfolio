19e7a61fa74611bd964a9e145fe4e288083690b9
jwilla <140156@unsaac.edu.pe>
2023-02-05 18:13:16 -0500

    padding

--- components/sections/intro/Intro.tsx
+++ components/sections/intro/Intro.tsx
@@ -3,7 +3,7 @@ import styles from './intro.module.css'
 
 const Intro = () => {
   return (
-    <section className="pt-32 py-12 sm:min-h-screen flex items-center sm:px-10 md:px-20">
+    <section className="pt-32 py-12 sm:min-h-screen flex items-center sm:px-10 md:px-20 px-4 md:max-w-7xl m-auto">
       <div className="w-auto sm:w-10/12">
         <h6 className="text-accent sm:text-lg font-bold font-mono">Hi there! I'm</h6>
         <h1 className='text-accent text-2xl sm:text-5xl font-black'>Johan Huaman.</h1>
