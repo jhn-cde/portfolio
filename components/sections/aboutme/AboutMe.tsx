f8ac5daddf3a991222caa9f4d3d3b37b1f48a437
jwilla <140156@unsaac.edu.pe>
2023-02-05 18:12:21 -0500

    styles

--- components/sections/aboutme/AboutMe.tsx
+++ components/sections/aboutme/AboutMe.tsx
@@ -5,18 +5,18 @@ import Profile from './profile/Profile'
 
 const AboutMe = () => {
   return (
-    <section className={styles.section}>
-    <div className={styles.container + ' container'}>
-      <div className={styles.profile}>
-        <Profile />
-      </div>
+    <section className='sm:min-h-screen bg-secondary lg:bg-secondary-light lg:bg-transparent'>
+      <div className="md:max-w-7xl m-auto px-4 py-12 lg:flex items-center sm:px md:px-20">
+        <div className='mb-8 lg:mb-0 lg:absolute'>
+          <Profile />
+        </div>
 
-      <div className={styles.description}>
-        <h2 className={styles.title} id={'aboutme'}>
-          About Me
-        </h2>
-        <Description />
-      </div>
+        <div className="lg:w-6/12 ml-auto lg:p-6 bg-opacity-0 lg:bg-secondary-light lg:bg-opacity-70 lg:backdrop-blur-md">
+          <h2 id={'aboutme'}>
+            About Me
+          </h2>
+          <Description />
+        </div>
       </div>
     </section>
   )
--- components/sections/aboutme/description/Description.tsx
+++ components/sections/aboutme/description/Description.tsx
@@ -3,35 +3,30 @@ import styles from './description.module.css'
 
 const Description = () => {
   return (
-    <div className={styles.container}>
-      <p className={styles.text}>
-        I'm from the National University San Antonio Abad del Cusco. I specialize in <span style={{fontWeight :'600'}}>software development</span> and <span style={{fontWeight :'600'}}>machine learning</span> and have gained valuable experience through participating in <span style={{fontWeight :'600'}}>coding competitions</span>.
+    <div className=''>
+      <p className="text-primary-light lg:text-secondary">
+        I'm from the National University San Antonio Abad del Cusco. I specialize in <span className="text-primary-light lg:text-secondary" style={{fontWeight :'600'}}>software development</span> and <span className="text-primary-light lg:text-secondary" style={{fontWeight :'600'}}>machine learning</span> and have gained valuable experience through participating in <span className="text-primary-light lg:text-secondary" style={{fontWeight :'600'}}>coding competitions</span>.
       </p>
-      <p className={styles.text}>
+      <p className="text-primary-light lg:text-secondary">
         I am highly motivated with a passion for technology and a strong desire to learn and grow as a professional. I am a fast learner, adaptable and always willing to take on new challenges.
       </p>
-      <p className={styles.text+' '+styles.hide}>
+      <p className="text-secondary hidden lg:block">
         As a recent graduate, I am eager to start my professional career and utilize the skills and knowledge I have acquired during my studies. I am open to remote or on-site opportunities and am willing to work from anywhere.
       </p>
-      <p className={styles.text}>
+      <p className="text-primary-light lg:text-secondary">
         Technologies I've been working with recently:
       </p>
-      <div className={styles.list_container}>
-        <div className={styles.list}>
-          <ul>
-            <li><span className={styles.item}>_</span> TypeScript</li>
-            <li><span className={styles.item}>_</span> React</li>
-            <li><span className={styles.item}>_</span> Express JS</li>
-          </ul>
-        </div>
-
-        <div className={styles.list}>
-          <ul>
-            <li><span className={styles.item}>_</span> Python</li>
-            <li><span className={styles.item}>_</span> C++</li>
-            <li><span className={styles.item}>_</span> Linux</li>
-          </ul>
-        </div>
+      <div className="text-accent flex">
+        <ul className="mr-12">
+          <li><span>_</span> TypeScript</li>
+          <li><span>_</span> React</li>
+          <li><span>_</span> Express JS</li>
+        </ul>
+        <ul>
+          <li><span>_</span> Python</li>
+          <li><span>_</span> C++</li>
+          <li><span>_</span> Linux</li>
+        </ul>
       </div>
     </div>
   )
