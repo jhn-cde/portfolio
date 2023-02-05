ac63d51e4afcbee22974cbb3fd65fd025415b167
jwilla <140156@unsaac.edu.pe>
2023-02-05 14:47:32 -0500

    main styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Intro from '@/components/intro/Intro'
+import Intro from '@/components/sections/intro/Intro'
 import Layout from '@/components/layout/Layout'
-import AboutMe from '@/components/aboutme/AboutMe'
-import MyProjects from '@/components/myprojects/MyProjects'
+import AboutMe from '@/components/sections/aboutme/AboutMe'
+import MyProjects from '@/components/sections/myprojects/MyProjects'
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="bg-white px-4 md:max-w-7xl m-auto">
         <Intro />
 
         <AboutMe />
