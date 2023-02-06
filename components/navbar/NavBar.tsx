58e02276b96583faf07a4dbc455271688454583e
jwilla <140156@unsaac.edu.pe>
2023-02-05 19:46:33 -0500

    hide scrollup

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -3,31 +3,61 @@ import React, { useEffect, useState } from 'react'
 import styles from './navbar.module.css'
 import { BsFillMoonStarsFill } from 'react-icons/bs'
 
+const truescroll = 70
+
 const NavBar = () => {
   const [logo, setLogo] = useState('/images/logo.svg')
-  const [scroll, setScroll] = useState(false)
+  const [scroll, setScroll] = useState(0)
+  const [scrollUp, setScrollUp] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll)
-  }, [])
+    const threshold = 0;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
 
-  const handleScroll = () => {
-    setScroll(window.scrollY>=70)
-    if (window.scrollY>=70)
-      setLogo('/images/logo_scroll.svg')
-    else
-      setLogo('/images/logo.svg')
-  }
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+  
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+      const isUp = scrollY < lastScrollY
+      if((!scrollUp && isUp)||(scrollUp && !isUp) || scrollY===0){
+        setScrollUp(isUp && scrollY!==0);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+
+      setScroll(window.scrollY)
+
+      if (window.scrollY>=truescroll)
+        setLogo('/images/logo_scroll.svg')
+      else
+        setLogo('/images/logo.svg')
+    };
+
+    window.addEventListener("scroll", onScroll);
+    
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollUp])
 
   return (
-    <nav className={'py-5 px-4 sm:px-10 w-screen flex justify-between items-center fixed top-0 z-50'.concat(scroll?' shadow-md shadow-blue-500/40 bg-opacity-50 bg-white backdrop-blur-md':'')}>
+    <nav className={(scrollUp && scroll>=truescroll?' hidden':'flex py-5 px-4 sm:px-10 w-screen justify-between items-center fixed top-0 z-50').concat(scroll>=truescroll?' shadow-md shadow-blue-500/40 bg-opacity-50 bg-white backdrop-blur-md':'')}>
       <div className='shrink-0'>
         <Image
           src={logo}
           width={50}
           height={50}
           alt='plan de estudios'
-          className={'w-auto '.concat(scroll?'h-6 sm:h-10':'h-8 sm:h-12')}
+          className={'w-auto '.concat(scroll>=truescroll?'h-6 sm:h-10':'h-8 sm:h-12')}
         />
       </div>
       <ul className='flex items-center'>
