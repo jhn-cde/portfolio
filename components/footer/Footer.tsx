510e3b85517c826bf9fb221c5c5058387d29c1ad
jwilla <140156@unsaac.edu.pe>
2023-02-11 15:39:09 -0500

    footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,8 +3,12 @@ import styles from './footer.module.css';
 
 const Footer = () => {
   return (
-    <footer className={styles.footer} title='footer'>
-      Footer
+    <footer className="text-center p-2 relative z-50" title='footer'>
+      <p className='font-nor
+      '>
+        <a className="pl-2 pr-2 mr-2 border-r-2 border-gray-500 font-bold" href="https://github.com/jhn-cde">jhn-cde</a>
+        Made with <a className='font-bold' href="https://github.com/jhn-cde/portfolio">next.js</a>
+      </p>
     </footer>
   )
 }
