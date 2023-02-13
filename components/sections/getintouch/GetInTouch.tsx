5d5e9daed81d87a6d2091641bbcfd73b173537a9
jwilla <140156@unsaac.edu.pe>
2023-02-13 15:12:34 -0500

    padding

--- components/sections/getintouch/GetInTouch.tsx
+++ components/sections/getintouch/GetInTouch.tsx
@@ -6,7 +6,7 @@ const GetInTouch = () => {
       <div className="md:max-w-2xl m-auto px-4 pt-0 py-16 lg:flex items-center sm:px md:px-20">
         <div className="text-center">
           <h2 className='pb-4'>Get in Touch</h2>
-          <p className='pb-4'>Thanks for taking the time to review my portfolio. I'm happy to connect with colleagues or employers in the industry. I look forward to hearing from you.</p>
+          <p className='pb-6'>Thanks for taking the time to review my portfolio. I'm happy to connect with colleagues or employers in the industry. I look forward to hearing from you.</p>
           <div>
             <a className="text-accent-dark underline" href="mailto:toe.johan@gmail.com" rel="noopener noreferrer" target="_blank">toe.johan@gmail.com</a>
           </div>
