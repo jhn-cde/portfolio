4d9005de8df092257aa58a64a09d0981a77f2819
jwilla <140156@unsaac.edu.pe>
2023-02-14 07:36:22 -0500

    next config

---
+++ next.config.js
@@ -0,0 +1,28 @@
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+let images = {}
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+
+  images = {
+    images: {
+      loader: 'imgix',
+      path: 'https://jhn-cde.github.io/portfolio/public'
+    }
+  }
+}
+
+module.exports = {
+  ...images,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
