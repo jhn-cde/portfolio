37a37d4eb6de61f58cc848ddef2fe52b84392885
jwilla <140156@unsaac.edu.pe>
2023-02-14 12:00:36 -0500

    get Projects

---
+++ lib/projects.tsx
@@ -0,0 +1,27 @@
+import { readdirSync, readFileSync } from 'fs';
+import matter from 'gray-matter';
+import { join } from 'path';
+
+const projectDir = join(process.cwd(), 'content')
+
+export function getProjects(){
+  const fileNames = readdirSync(projectDir);
+  let allProjects = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, '');
+
+    const fullPath = join(projectDir, fileName);
+    const fileContents = readFileSync(fullPath, 'utf8');
+
+    const matterResult = matter(fileContents)
+
+    return{
+      id,
+      ...matterResult
+    }    
+  })
+
+  allProjects = allProjects.filter((project) => project.data.display)
+  return allProjects.sort((a, b) => {
+    return a.data.num < b.data.num ? -1 : 1
+  })
+}
---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.fallback = {fs: false};
+    return config;
+  }
+}
