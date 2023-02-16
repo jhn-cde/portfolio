aac82d31936afc4df4f7297b3531211dfef3351b
jwilla <140156@unsaac.edu.pe>
2023-02-16 14:19:48 -0500

    height

--- components/sections/getintouch/GetInTouch.tsx
+++ components/sections/getintouch/GetInTouch.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GetInTouch = () => {
   return (
     <section className="flex items-center" id='getintouch'>
-      <div className="md:max-w-2xl m-auto px-4 pt-0 py-16 lg:flex items-center sm:px md:px-20">
+      <div className="md:max-w-2xl m-auto px-4 pt-0 py-16 lg:flex items-center sm:px md:px-20 lg:h-96 lg:my-4">
         <div className="text-center">
           <h2 className='pb-4'>Get in Touch</h2>
           <p className='pb-6'>Thanks for taking the time to review my portfolio. I'm happy to connect with colleagues or employers in the industry. I look forward to hearing from you.</p>
