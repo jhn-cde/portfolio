3b6647614726e5bf520ef10906324069b26f45f8
jwilla <140156@unsaac.edu.pe>
2023-02-17 13:55:36 -0500

    year

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className="text-center p-2 w-screen z-50 bg-primary-1 -mt-1 dark:bg-primary-dark-3">
       <p className='text-sm text-secondary-1 dark:text-secondary-dark-1'>
-        <a className="pl-2 pr-2 mr-2 border-r-2 border-secondary-1 font-bold dark:border-secondary-dark-1" href="https://github.com/jhn-cde">jhn-cde</a>
-        Made with <a className='font-bold' href="https://github.com/jhn-cde/portfolio">next.js</a>
+        <a className="pl-2 pr-2 mr-2 border-r-2 border-secondary-1 font-bold dark:border-secondary-dark-1" href="https://github.com/jhn-cde">jhn-cde - 2023</a>
+        Made with <a className='font-bold' href="https://github.com/jhn-cde/portfolio" target={'_blank'}>next.js</a>
       </p>
     </div>
   )
