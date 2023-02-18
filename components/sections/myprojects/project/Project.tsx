dcab23101c6a4b5d2a7da617e709ceb229a682ee
jwilla <140156@unsaac.edu.pe>
2023-02-18 14:29:52 -0500

    techs styles

--- components/sections/myprojects/project/Project.tsx
+++ components/sections/myprojects/project/Project.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
-import { FaExternalLinkAlt, FaGithub, FaLink } from "react-icons/fa";
-import styles from './project.module.css'
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+import { SiCplusplus, SiCsharp, SiExpress, SiGooglecolab, SiJavascript, SiKeras, SiMysql, SiPython, SiReact, SiTypescript } from 'react-icons/si';
+import { TbBrandReactNative } from 'react-icons/tb';
+import { DiMsqlServer, DiMysql } from 'react-icons/di';
 
 interface Props{
   title: string,
@@ -15,6 +17,37 @@ interface Props{
 }
 
 const Project = ({title, description, p_type, techs, img, gh, url, type}: Props) => {
+  const getTechIcon = (tech: string) => {
+    switch (tech) {
+      case 'JavaScript':
+        return <SiJavascript className='mr-3'/>
+      case 'TypeScript':
+        return <SiTypescript className='mr-3'/>
+      case 'React':
+        return <SiReact className='mr-3'/>
+      case 'ReactNative':
+        return <TbBrandReactNative className='mr-3'/>
+      case 'SQLServer':
+        return <DiMsqlServer className='mr-3'/>
+      case 'SQLLite':
+        return <DiMysql className='mr-3'/>
+      case 'Python':
+        return <SiPython className='mr-3'/>
+      case 'Cpp':
+        return <SiCplusplus className='mr-3'/>
+      case 'CSharp':
+        return <SiCsharp className='mr-3'/>
+      case 'Keras':
+        return <SiKeras className='mr-3'/>
+      case 'Colab':
+        return <SiGooglecolab className='mr-3'/>
+      case 'Express':
+        return <SiExpress className='mr-3'/>
+      default:
+        break;
+    }
+  }
+
   return (
     <div 
       className='relative rounded-md md:h-80 lg:h-96'
@@ -36,39 +69,32 @@ const Project = ({title, description, p_type, techs, img, gh, url, type}: Props)
         />
       </div>
       <div 
-        className={'relative z-10 h-full bg-primary-dark py-6 text-secondary-dark my-auto bg-opacity-95 backdrop-blur-sm flex items-center md:w-5/12 md:bg-opacity-0'.concat(
+        className={'relative z-10 h-full bg-primary-dark text-secondary-dark my-auto bg-opacity-100 rounded-md py-10 md:py-0 md:backdrop-blur-sm flex items-center md:w-5/12 md:bg-opacity-0'.concat(
           type==='r' ? ' md:ml-auto md:text-right' : ''
         )}
       >
         <div className='w-full'>
           <p className={'px-10 font-normal text-accent-dark'.concat(type==='r' ? ' md:pl-0' : '')}>{p_type}</p>
-          <h3 className={'px-10 text-secondary-dark-3 md:text-secondary md:dark:text-secondary-dark-3'.concat(type==='r' ? ' md:pl-0' : '')}>{title}</h3>
-          <div
-            className={'flex flex-wrap px-10 mb-3 text-accent-dark font-mono md:px-0'.concat(
-              type==='r' ? ' md:justify-end md:pl-0 md:pr-10' : ' md:pl-10 md:pr-0'
-          )}>
-            {techs.split(', ').map(item => (
-              <p 
-                key={item} 
-                className={'mr-3'.concat(
-                  type==='r' ? ' md:mr-0 md:ml-4' : ' md:mr-4'
-              )}>{item}</p>
-            ))}
-          </div>
+          <h3 className={'px-10 text-secondary-dark-3 md:text-secondary pb-5 md:pb-0 md:dark:text-secondary-dark-3'.concat(type==='r' ? ' md:pl-0' : '')}>{title}</h3>
           <div className={'relative mb-4 bg-primary-dark-1 bg-opacity-0 md:py-6 md:bg-opacity-95 md:rounded-md md:backdrop-blur-xl md:bg-primary-dark md:absolute'.concat(
             type==='r' ? ' md:-left-3/4' : ' md:-right-1/2'
           )}>
             <p 
-              className={'px-10 mb-2'.concat(
+              className={'px-10 mb-2 pb-4'.concat(
                 type==='r' ? '' : ' md:pl-10'
               )}>{description}</p>
             <div 
-              className={'flex flex-wrap px-10 text-accent font-mono'.concat(
+              className={'flex items-center flex-wrap px-10 font-mono'.concat(
                 type==='r' ? ' md:justify-end' : ''
             )}>
+              {techs.split(', ').map(item => (
+              <div className='flex items-center text-lg text-white' key={item}>
+                {getTechIcon(item)}
+              </div>))}
+              {url || gh && '|'}
               {gh&&(
                 <a 
-                  className={'mr-3 text-xl'.concat(
+                  className={'ml-3 text-xl text-accent hover:text-accent-light hover:text-2xl'.concat(
                     type==='r' ? ' md:mr-0 md:ml-3' : ''
                   )} 
                   href={gh} 
@@ -78,9 +104,9 @@ const Project = ({title, description, p_type, techs, img, gh, url, type}: Props)
                   <FaGithub />
                 </a>
               )}
-              {url&& (
+              {url && (
                 <a 
-                  className='ml-3 text-xl' 
+                  className='ml-3 text-xl text-accent hover:text-accent-light hover:text-2xl' 
                   href={url} 
                   target={'_blank'}
                   aria-label={`url`}
--- content/tutoring_system.md
+++ content/tutoring_system.md
@@ -2,7 +2,7 @@
 title: 'Tutoring System'
 description: 'A group project to improve teamwork skills using SCRUM, Desktop application for organization and distribution of tutor teachers at the university.'
 p_type: 'University Project'
-techs: 'C#, SQL Server, Visual Studio 2019'
+techs: 'CSharp, SQLServer'
 gh: 'https://github.com/Jeremylazm/AppSistemaTutoria'
 url: ''
 img: 'tutoring.webp'
